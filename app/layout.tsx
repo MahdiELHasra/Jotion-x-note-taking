78ae5c3b741d96845eb2af5efa5f6a751ef43d15
El hasra mahdi <114767917+MehdiEL7asra@users.noreply.github.com>
2024-01-09 20:54:42 +0100

    Add Convex and Clerk dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
 import type { Metadata } from "next";
-import { ThemeProvider } from "../components/providers/theme-provider";
 import { Inter } from "next/font/google";
+import { ThemeProvider } from "../components/providers/theme-provider";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -33,15 +34,17 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-          storageKey="jotion-theme"
-        >
-          {children}
-        </ThemeProvider>
+        <ConvexClientProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+            storageKey="jotion-theme"
+          >
+            {children}
+          </ThemeProvider>
+        </ConvexClientProvider>
       </body>
     </html>
   );
