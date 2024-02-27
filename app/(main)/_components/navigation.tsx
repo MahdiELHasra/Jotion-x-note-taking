e59edcc381931e03a69ad9b215cc9fa650c75c56
El hasra mahdi <114767917+MehdiEL7asra@users.noreply.github.com>
2024-02-27 21:16:43 +0100

    feat: Add Navbar component with collapsible functionality

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -9,7 +9,7 @@ import {
   Search,
   Settings,
 } from "lucide-react";
-import { usePathname } from "next/navigation";
+import { useParams, usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 
@@ -32,10 +32,12 @@ import { UserItem } from "./user-item";
 import { Item } from "./item";
 import { DocumentList } from "./document-list";
 import { TrashBox } from "./trash-box";
+import { Navbar } from "./navbar";
 
 export const Navigation = () => {
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
+  const params = useParams();
   const create = useMutation(api.documents.create);
   const search = useSearch();
   const settings = useSettings();
@@ -195,6 +197,19 @@ export const Navigation = () => {
             />
           )}
         </nav>
+        {!!params.documentId ? (
+          <Navbar isCollapsed={isCollapsed} onResetWidth={resetWidth} />
+        ) : (
+          <nav className="bg-transparent px-3 py-2 w-full">
+            {isCollapsed && (
+              <MenuIcon
+                onClick={resetWidth}
+                role="button"
+                className="h-6 w-6 text-muted-foreground"
+              />
+            )}
+          </nav>
+        )}
       </div>
     </>
   );
