ac63c28953dca5b925f7e29a2fcc56328a305c12
El hasra mahdi <114767917+MehdiEL7asra@users.noreply.github.com>
2024-03-03 12:07:09 +0100

    refactor: fix typo in font size in Heading

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,26 +1,28 @@
 "use client";
 
-import { Spinner } from "@/components/spinner";
-import { Button } from "@/components/ui/button";
-import { SignInButton } from "@clerk/clerk-react";
 import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
+import { SignInButton } from "@clerk/clerk-react";
 import Link from "next/link";
 
-const Heading = () => {
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+
+export const Heading = () => {
   const { isAuthenticated, isLoading } = useConvexAuth();
+
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
         Your Ideas, Documents, & Plans. Unified. Welcome to{" "}
         <span className="underline">Jotion</span>
       </h1>
-      <h3 className="text-base sm:text-xl md:text-2xl font-meduim">
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
         Jotion is the connected workspace where <br />
         better, faster work happens.
       </h3>
       {isLoading && (
-        <div className="w-full flex items-center justify-center ">
+        <div className="w-full flex items-center justify-center">
           <Spinner size="lg" />
         </div>
       )}
@@ -28,7 +30,7 @@ const Heading = () => {
         <Button asChild>
           <Link href="/documents">
             Enter Jotion
-            <ArrowRight className="w-4 h-4 ml-2" />
+            <ArrowRight className="h-4 w-4 ml-2" />
           </Link>
         </Button>
       )}
@@ -43,4 +45,3 @@ const Heading = () => {
     </div>
   );
 };
-export default Heading;
