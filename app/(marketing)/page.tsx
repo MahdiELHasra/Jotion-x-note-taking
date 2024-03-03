231edc7eee7f8e03396226c359b697f4739702e4
El hasra mahdi <114767917+MehdiEL7asra@users.noreply.github.com>
2024-03-03 12:05:53 +0100

    refactor: fix import statements in Marketing Page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,11 +1,11 @@
-import Footer from "./_components/footer";
-import Heading from "./_components/heading";
-import Heroes from "./_components/heroes";
+import { Footer } from "./_components/footer";
+import { Heading } from "./_components/heading";
+import { Heroes } from "./_components/heroes";
 
 const MarketingPage = () => {
   return (
     <div className="min-h-full flex flex-col dark:bg-[#1F1F1F]">
-      <div className="flex dark:bg-[#1f1f1f] flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
         <Heading />
         <Heroes />
       </div>
@@ -13,4 +13,5 @@ const MarketingPage = () => {
     </div>
   );
 };
+
 export default MarketingPage;
